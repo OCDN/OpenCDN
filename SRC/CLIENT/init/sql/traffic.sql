d994e1bd498f1fd20110c0f1c9cd53d9d7624105
Twwy <git@twwy.net>
2014-11-08 11:28:19 +0800

    sql update

--- SRC/CLIENT/init/sql/traffic.sql
+++ SRC/CLIENT/init/sql/traffic.sql
@@ -1,14 +1,16 @@
-CREATE TABLE data(
+CREATE TABLE IF NOT EXISTS data(
 	app_id INTEGER,
 	time INTEGER,
 	domain TEXT,
 	pv INTEGER,
-	hit_pv INTEGER,
+	pv_hit INTEGER,
 	byte INTEGER,
-	hit_byte INTEGER,
+	byte_hit INTEGER,
+	byte_req INTEGER,
+	time_req INTEGER,
 	pv_2xx INTEGER,
 	pv_3xx INTEGER,
 	pv_4xx INTEGER,
 	pv_5xx INTEGER,
-	PRIMARY KEY(app_id, time)
+	PRIMARY KEY(app_id, time, domain)
 );
